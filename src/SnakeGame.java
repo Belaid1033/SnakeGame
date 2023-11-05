277bf56ce905ce66805c6f1932e5f5505dc2c358
belaid1033 <87024200+Belaid1033@users.noreply.github.com>
2023-11-05 23:47:14 +0100

    add some methodes to the SnakeGame class

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -18,8 +18,14 @@ public class SnakeGame extends JPanel{
     int boardWidth;
     int boardHeight;
     int tileSize=25;
-
+    //snake
     Tile snakeHead;
+
+    //food
+    Tile food;
+    Random random;
+
+
     SnakeGame(int boardWidth,int boardHeight){
         this.boardHeight=boardHeight;
         this.boardWidth=boardWidth;
@@ -27,6 +33,9 @@ public class SnakeGame extends JPanel{
         setBackground(Color.black);
 
         snakeHead=new Tile(5,5);
+        food=new Tile(10,10);
+        random=new Random();
+        placeFood();
     }
 
     public void paintComponent(Graphics g){
@@ -35,9 +44,20 @@ public class SnakeGame extends JPanel{
     }
 
     public void draw(Graphics g){
+        //grid
+        for(int i=0;i< boardWidth/tileSize;i++){
+            g.drawLine(i*tileSize,0,i*tileSize, boardHeight);
+            g.drawLine(0,i*tileSize, boardWidth,i*tileSize);
+        }
+        //food
+        g.setColor(Color.green);
+        g.fillRect(food.x*tileSize,food.y*tileSize,tileSize,tileSize);
         //snake
         g.setColor(Color.white);
         g.fillRect(snakeHead.x*tileSize,snakeHead.y*tileSize,tileSize,tileSize);
     }
-
+    public void placeFood(){
+        food.x= random.nextInt(boardWidth/tileSize);
+        food.y= random.nextInt(boardHeight/tileSize);
+    }
 }
